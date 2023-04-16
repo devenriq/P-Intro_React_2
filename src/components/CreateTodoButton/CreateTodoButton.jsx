12883cfd1c0bca98aa0c768d9cd578842172102a
enrique palomino <octepc.97@gmail.com>
2023-04-15 20:24:15 -0500

    style: :art: Stilyzed the button to add a todo

--- src/components/CreateTodoButton/CreateTodoButton.jsx
+++ src/components/CreateTodoButton/CreateTodoButton.jsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+
 export const CreateTodoButton = () => {
-  return <div>CreateTodoButton</div>;
+  const [button, setButton] = useState([]);
+
+  const handleAdd = () => {
+    console.log("click");
+  };
+
+  return (
+    <div className="flex justify-center items-center relative ">
+      <button
+        className="bg-red-400 w-16 h-16 rounded-full flex-shrink-0  fixed z-50 bottom-10 right-10 hover:w-20 hover:h-20"
+        onClick={handleAdd}
+      >
+        <span className="text-5xl hover:text-7xl text-white">+</span>
+      </button>
+    </div>
+  );
 };
