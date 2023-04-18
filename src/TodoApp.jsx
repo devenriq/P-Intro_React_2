802066fbfdb8bc4fcde0da98d2b4b3b40a1e32f9
enrique palomino <octepc.97@gmail.com>
2023-04-17 20:31:34 -0500

    feat: :sparkles: Using useContext to serve the info required

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -14,37 +14,18 @@ import { TodoContext } from "./components/context/TodoContext";
 import "../dist/index.css";
 
 function TodoApp() {
-  const {} = useContext(TodoContext);
   const {
-    item: todos,
-    saveItem: saveTodos,
+    todos,
+    saveTodos,
     loading,
     error,
-  } = useLocalStorage("TODOS_V1", []);
-  const [searchValue, setSearchValue] = useState("");
-
-  useEffect(() => {
-    console.log("use effect");
-  }, [todos]);
-
-  const toggleTodoCompleted = (title) => {
-    const todoIndex = todos.findIndex((todo) => todo.title === title);
-    const newTodos = [...todos];
-    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
-
-    saveTodos(newTodos);
-  };
-
-  const toggleTodoDelete = (title) => {
-    const todoIndex = todos.findIndex((todo) => todo.title === title);
-    const newTodos = [...todos];
-    newTodos.splice(todoIndex, 1);
-
-    saveTodos(newTodos);
-  };
-
-  const completedTodo = todos.filter((todo) => !!todo.completed).length;
-  const pendingTodo = todos.filter((todo) => !todo.completed).length;
+    searchValue,
+    setSearchValue,
+    toggleTodoCompleted,
+    toggleTodoDelete,
+    completedTodo,
+    pendingTodo,
+  } = useContext(TodoContext);
 
   return (
     <TodoProvider>
--- src/components/context/TodoProvider.jsx
+++ src/components/context/TodoProvider.jsx
@@ -1,52 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { TodoContext } from "./TodoContext";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 export const TodoProvider = ({ children }) => {
-  // const {
-  //   item: todos,
-  //   saveItem: saveTodos,
-  //   loading,
-  //   error,
-  // } = useLocalStorage("TODOS_V1", []);
-  // const [searchValue, setSearchValue] = useState("");
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage("TODOS_V1", []);
+  const [searchValue, setSearchValue] = useState("");
 
-  // useEffect(() => {
-  //   console.log("use effect");
-  // }, [todos]);
+  useEffect(() => {
+    console.log("use effect");
+  }, [todos]);
 
-  // const toggleTodoCompleted = (title) => {
-  //   const todoIndex = todos.findIndex((todo) => todo.title === title);
-  //   const newTodos = [...todos];
-  //   newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+  const toggleTodoCompleted = (title) => {
+    const todoIndex = todos.findIndex((todo) => todo.title === title);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
 
-  //   saveTodos(newTodos);
-  // };
+    saveTodos(newTodos);
+  };
 
-  // const toggleTodoDelete = (title) => {
-  //   const todoIndex = todos.findIndex((todo) => todo.title === title);
-  //   const newTodos = [...todos];
-  //   newTodos.splice(todoIndex, 1);
+  const toggleTodoDelete = (title) => {
+    const todoIndex = todos.findIndex((todo) => todo.title === title);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
 
-  //   saveTodos(newTodos);
-  // };
+    saveTodos(newTodos);
+  };
 
-  // const completedTodo = todos.filter((todo) => !!todo.completed).length;
-  // const pendingTodo = todos.filter((todo) => !todo.completed).length;
+  const completedTodo = todos.filter((todo) => !!todo.completed).length;
+  const pendingTodo = todos.filter((todo) => !todo.completed).length;
 
   return (
     <TodoContext.Provider
-      value={
-        {
-          // todos,
-          // saveTodos,
-          // loading,
-          // error,
-          // toggleTodoCompleted,
-          // toggleTodoDelete,
-          // completedTodo,
-          // pendingTodo,
-        }
-      }
+      value={{
+        todos,
+        saveTodos,
+        loading,
+        error,
+        searchValue,
+        setSearchValue,
+        toggleTodoCompleted,
+        toggleTodoDelete,
+        completedTodo,
+        pendingTodo,
+      }}
     >
       {children}
     </TodoContext.Provider>
